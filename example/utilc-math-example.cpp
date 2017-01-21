7f76285617c8221210b1847439c7c5fda70027bc
Cameron A. Craig <camieac@gmail.com>
2017-01-21 23:54:36 +0000

    Improved example.

--- example/utilc-math-example.cpp
+++ example/utilc-math-example.cpp
@@ -18,45 +18,73 @@ using namespace ucm;
 
 int main (int argc, char *argv[]){
 	//Define two vectors to be used throughout the examples.
-	vec3 v1(0.1f, 0.1f, 0.1f);
-	cout << "v1 = " << v1.toString() << endl;
-
-	vec3 v2;
-	cout << "v2 = " << v2.toString() << endl;
+	vec3 ones(1.0f, 1.0f, 1.0f);
+	cout << "ones = " << ones.toString() << endl;
+	vec3 zeros; //Default constructor initialises with zeros
+	cout << "zeros = " << zeros.toString() << endl;
 
 	//Example vector arithmetic
-	vec3 sum = v1 + v2;
+	cout << endl << "Vector Arithmetic:" << endl;
 
-	vec3 dif = v1 - v2;
+	vec3 sum = ones + ones;
+	cout << "ones + ones = " << sum.toString() << endl;
 
-	float dot_prod = v1.dot(v2);
+	sum += 1.0f;
+	sum -= 1.0f;
+	sum *= 1.0f;
+	sum /= 1.0f;
 
-	vec3 cross_prod = v1.cross(v2);
+	vec3 diff = ones - ones;
+	cout << "ones - ones = " << diff.toString() << endl;
+
+	float dot_prod = vec3(12.0f, 8.0f, 5.0f).dot(vec3(2.0f, 3.0f, 4.0f));
+	cout << "(12,8,5).(2,3,4) == " << dot_prod << endl;
+
+	vec3 cross_prod = vec3(12.0f, 8.0f, 5.0f).cross(vec3(2.0f, 3.0f, 4.0f));
+	cout << "(12,8,5)x(2,3,4) == " << cross_prod.toString() << endl;
 
 	//Example scalar arithmetic
-	vec3 add = v1 + 3.0f;
+	cout << endl << "Scalar Arithmetic:" << endl;
 
-	vec3 sub = v1 - 1.0f;
+	vec3 add = ones + 3.0f;
+	cout << "ones + 3 = " << add.toString() << endl;
 
-	vec3 mul = v1 * 2.0f;
+	vec3 sub = ones - 1.0f;
+	cout << "ones - 1 = " << sub.toString() << endl;
 
-	vec3 div = v1 / 2.0f;
+	vec3 mul = ones * 2.0f;
+	cout << "ones * 2 = " << mul.toString() << endl;
 
-	 if (v1 == v2){
-		 cout << "v1 == v2";
-	 }else {
-		 cout << "v1 != v2";
-	 }
-	 //v1.length();
-	// vec3 v2();
-	//
-	// vec3 v3;// = v1 + v2;
+	vec3 div = ones / 2.0f;
+	cout << "ones / 2 = " << div.toString() << endl;
 
-	//cout << v3.length();
+	cout << endl << "Comparion:" << endl;
+	if (ones == zeros){
+		cout << "ones == zeros";
+	}else {
+		cout << "ones != zeros" << endl;
+	}
 
-	// cout << ucm_version();
-	cout << "\n";
+	cout << endl << "Length/Magnitude:" << endl;
+	float length = ones.length();
+	cout << "|ones| = " << length << endl;
 
+	cout << endl << "Member access:" << endl;
+	float x,y,z;
+	x = ones.x;
+	y = ones.y;
+	z = ones.z;
+	cout << "z:"<< x << ", y:" << y << ", z:" << z << endl;
+
+	x = ones.r;
+	y = ones.g;
+	z = ones.b;
+	cout << "r:"<< x << ", g:" << y << ", b:" << z << endl;
+
+	x = ones[0];
+	y = ones[1];
+	z = ones[2];
+	cout << "[0]:"<< x << ", [1]:" << y << ", [2]:" << z << endl;
 
 	return EXIT_SUCCESS;
 }
